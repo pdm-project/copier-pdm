d6aea17ac8d849e1959f4673b6a4f6598700c684
Frost Ming <mianghong@gmail.com>
2022-04-27 15:08:51 +0800

    Update the template

--- project/noxfile.py
+++ project/noxfile.py
@@ -5,7 +5,7 @@ import nox
 os.environ["PDM_IGNORE_SAVED_PYTHON"] = "1"
 
 
-@nox.session(python=("3.7", "3.8", "3.9"))
+@nox.session(python=("3.7", "3.8", "3.9", "3.10"))
 def test(session):
-    session.run("pdm", "install", "-d", external=True)
+    session.run("pdm", "install", "-Gtest", external=True)
     session.run("pytest", "tests/")
